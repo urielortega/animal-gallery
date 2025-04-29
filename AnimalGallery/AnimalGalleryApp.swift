813231157890521d2b5c42054d85025fdf1d6a65
Uriel Ortega <urielortega2509@gmail.com>
2025-04-29 12:34:42 -0600

    Initial Commit

---
+++ AnimalGallery/AnimalGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalGalleryApp.swift
+//  AnimalGallery
+//
+//  Created by Uriel Ortega on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimalGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
