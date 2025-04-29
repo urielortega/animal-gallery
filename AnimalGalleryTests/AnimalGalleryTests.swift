9014f10460a803a194a62f39b14731e91d1efb28
Uriel Ortega <urielortega2509@gmail.com>
2025-04-29 13:59:02 -0600

    Add Unit Testing target and files.

---
+++ AnimalGalleryTests/AnimalGalleryTests.swift
@@ -0,0 +1,13 @@
+//
+//  AnimalGalleryTests.swift
+//  AnimalGalleryTests
+//
+//  Created by Uriel Ortega on 29/04/25.
+//
+
+import XCTest
+@testable import AnimalGallery
+
+class AnimalGalleryTests: XCTestCase {
+    
+}
