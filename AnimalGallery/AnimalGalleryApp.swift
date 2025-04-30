10fd110e0935a07fbb55c88019e7ccc0cea53bd5
Uriel Ortega <urielortega2509@gmail.com>
2025-04-29 21:29:59 -0600

    Replace ContentView with GalleryView in AnimalGalleryApp.swift

--- AnimalGallery/AnimalGalleryApp.swift
+++ AnimalGallery/AnimalGalleryApp.swift
@@ -13,7 +13,7 @@ struct AnimalGalleryApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GalleryView()
                 .environmentObject(animalProvider)
         }
     }
