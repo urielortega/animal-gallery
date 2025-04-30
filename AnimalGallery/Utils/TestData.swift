7e2b3515545faffdbf72a91afffafe712ab78ff0
Uriel Ortega <urielortega2509@gmail.com>
2025-04-29 19:44:37 -0600

    Add TestData for XCTests.

---
+++ AnimalGallery/Utils/TestData.swift
@@ -0,0 +1,355 @@
+//
+//  TestData.swift
+//  AnimalGallery
+//
+//  Created by Uriel Ortega on 29/04/25.
+//
+
+import Foundation
+
+let testAnimalPhoto: Data = """
+      {
+        "id": "KdZvvyemkPM",
+        "slug": "red-and-brown-rooster-head-in-closeup-shot-KdZvvyemkPM",
+        "alternative_slugs": {
+          "en": "red-and-brown-rooster-head-in-closeup-shot-KdZvvyemkPM",
+          "es": "cabeza-de-gallo-rojo-y-marron-en-primer-plano-KdZvvyemkPM",
+          "ja": "クローズアップショットの赤と茶色のオンドリの頭-KdZvvyemkPM",
+          "fr": "tete-de-coq-rouge-et-brun-en-gros-plan-KdZvvyemkPM",
+          "it": "testa-di-gallo-rossa-e-marrone-in-primo-piano-KdZvvyemkPM",
+          "ko": "빨간색과-갈색-수탉-머리-클로즈업-샷-KdZvvyemkPM",
+          "de": "roter-und-brauner-hahnenkopf-in-nahaufnahme-KdZvvyemkPM",
+          "pt": "cabeca-de-galo-vermelha-e-marrom-em-foto-de-closeup-KdZvvyemkPM"
+        },
+        "created_at": "2016-04-30T14:40:48Z",
+        "updated_at": "2025-04-29T18:07:58Z",
+        "promoted_at": "2016-04-30T14:40:48Z",
+        "width": 5616,
+        "height": 3744,
+        "color": "#592626",
+        "blur_hash": "LSF|PlEM0}-nIqxZkBVuEfR+xGn%",
+        "description": "Rooster side view",
+        "alt_description": "red and brown rooster head in closeup shot",
+        "breadcrumbs": [],
+        "urls": {
+          "raw": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3",
+          "full": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?crop=entropy&cs=srgb&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=85",
+          "regular": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=1080",
+          "small": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=400",
+          "thumb": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=200",
+          "small_s3": "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1462027076063-1ceabb252dbd"
+        },
+        "links": {
+          "self": "https://api.unsplash.com/photos/red-and-brown-rooster-head-in-closeup-shot-KdZvvyemkPM",
+          "html": "https://unsplash.com/photos/red-and-brown-rooster-head-in-closeup-shot-KdZvvyemkPM",
+          "download": "https://unsplash.com/photos/KdZvvyemkPM/download?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8",
+          "download_location": "https://api.unsplash.com/photos/KdZvvyemkPM/download?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8"
+        },
+        "likes": 472,
+        "liked_by_user": false,
+        "current_user_collections": [],
+        "sponsorship": null,
+        "topic_submissions": {
+          "animals": {
+            "status": "approved",
+            "approved_on": "2020-11-03T10:41:03Z"
+          }
+        },
+        "asset_type": "photo",
+        "user": {
+          "id": "Qjmy5alzMGo",
+          "updated_at": "2025-02-26T01:18:47Z",
+          "username": "awesome",
+          "name": "Ashes Sitoula",
+          "first_name": "Ashes",
+          "last_name": "Sitoula",
+          "twitter_username": null,
+          "portfolio_url": null,
+          "bio": "introvert",
+          "location": "Nepal",
+          "links": {
+            "self": "https://api.unsplash.com/users/awesome",
+            "html": "https://unsplash.com/@awesome",
+            "photos": "https://api.unsplash.com/users/awesome/photos",
+            "likes": "https://api.unsplash.com/users/awesome/likes",
+            "portfolio": "https://api.unsplash.com/users/awesome/portfolio",
+            "following": "https://api.unsplash.com/users/awesome/following",
+            "followers": "https://api.unsplash.com/users/awesome/followers"
+          },
+          "profile_image": {
+            "small": "https://images.unsplash.com/profile-1722685586012-530c109255ccimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=32&h=32",
+            "medium": "https://images.unsplash.com/profile-1722685586012-530c109255ccimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=64&h=64",
+            "large": "https://images.unsplash.com/profile-1722685586012-530c109255ccimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=128&h=128"
+          },
+          "instagram_username": null,
+          "total_collections": 0,
+          "total_likes": 4,
+          "total_photos": 565,
+          "total_promoted_photos": 8,
+          "total_illustrations": 0,
+          "total_promoted_illustrations": 0,
+          "accepted_tos": true,
+          "for_hire": false,
+          "social": {
+            "instagram_username": null,
+            "portfolio_url": null,
+            "twitter_username": null,
+            "paypal_email": null
+          }
+        },
+        "exif": {
+          "make": "Canon",
+          "model": "Canon EOS 5D Mark II",
+          "name": "Canon, EOS 5D Mark II",
+          "exposure_time": "1/400",
+          "aperture": "5.6",
+          "focal_length": "400.0",
+          "iso": 320
+        },
+        "location": {
+          "name": "Kathmandu, Nepal",
+          "city": "Kathmandu",
+          "country": "Nepal",
+          "position": {
+            "latitude": 27.7172453,
+            "longitude": 85.3239605
+          }
+        },
+        "views": 7440191,
+        "downloads": 58296
+      }
+    """.data(using: .utf8)!
+
+let testAnimalPhotos: Data = """
+    [
+      {
+        "id": "EyWp41EftT0",
+        "slug": "brown-lion-lying-on-green-grass-during-daytime-EyWp41EftT0",
+        "alternative_slugs": {
+          "en": "brown-lion-lying-on-green-grass-during-daytime-EyWp41EftT0",
+          "es": "leon-marron-acostado-sobre-hierba-verde-durante-el-dia-EyWp41EftT0",
+          "ja": "昼間緑の芝生に横たわる茶色のライオン-EyWp41EftT0",
+          "fr": "lion-brun-couche-sur-lherbe-verte-pendant-la-journee-EyWp41EftT0",
+          "it": "leone-marrone-sdraiato-sullerba-verde-durante-il-giorno-EyWp41EftT0",
+          "ko": "낮에는-푸른-잔디에-누워-있는-갈색-사자-EyWp41EftT0",
+          "de": "brauner-lowe-der-tagsuber-auf-grunem-gras-liegt-EyWp41EftT0",
+          "pt": "leao-marrom-deitado-na-grama-verde-durante-o-dia-EyWp41EftT0"
+        },
+        "created_at": "2020-10-25T13:40:40Z",
+        "updated_at": "2025-04-29T18:57:32Z",
+        "promoted_at": "2020-10-26T10:57:02Z",
+        "width": 3185,
+        "height": 3981,
+        "color": "#262626",
+        "blur_hash": "LA8N5^9b56~A-VIpR*%20fxZ={EM",
+        "description": "Lion being rudely awakened by a noisy hyena",
+        "alt_description": "brown lion lying on green grass during daytime",
+        "breadcrumbs": [],
+        "urls": {
+          "raw": "https://images.unsplash.com/photo-1603632088148-3680d7e5dda2?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3",
+          "full": "https://images.unsplash.com/photo-1603632088148-3680d7e5dda2?crop=entropy&cs=srgb&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=85",
+          "regular": "https://images.unsplash.com/photo-1603632088148-3680d7e5dda2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=1080",
+          "small": "https://images.unsplash.com/photo-1603632088148-3680d7e5dda2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=400",
+          "thumb": "https://images.unsplash.com/photo-1603632088148-3680d7e5dda2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=200",
+          "small_s3": "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1603632088148-3680d7e5dda2"
+        },
+        "links": {
+          "self": "https://api.unsplash.com/photos/brown-lion-lying-on-green-grass-during-daytime-EyWp41EftT0",
+          "html": "https://unsplash.com/photos/brown-lion-lying-on-green-grass-during-daytime-EyWp41EftT0",
+          "download": "https://unsplash.com/photos/EyWp41EftT0/download?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8",
+          "download_location": "https://api.unsplash.com/photos/EyWp41EftT0/download?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8"
+        },
+        "likes": 755,
+        "liked_by_user": false,
+        "current_user_collections": [],
+        "sponsorship": null,
+        "topic_submissions": {
+          "animals": {
+            "status": "approved",
+            "approved_on": "2021-04-23T13:50:25Z"
+          },
+          "nature": {
+            "status": "approved",
+            "approved_on": "2020-10-26T10:52:27Z"
+          },
+          "wallpapers": {
+            "status": "approved",
+            "approved_on": "2021-04-23T13:50:27Z"
+          }
+        },
+        "asset_type": "photo",
+        "user": {
+          "id": "D5No1qiMyxU",
+          "updated_at": "2024-10-11T04:00:58Z",
+          "username": "eadesstudio",
+          "name": "James Eades",
+          "first_name": "James",
+          "last_name": "Eades",
+          "twitter_username": "eadesstudio",
+          "portfolio_url": "http://eades.studio",
+          "bio": "Feel free to visit my website or Instagram (@jmeeades) for more shots! Please contact me if you wish to use images for prints/commercial use.",
+          "location": "United Kingdom",
+          "links": {
+            "self": "https://api.unsplash.com/users/eadesstudio",
+            "html": "https://unsplash.com/@eadesstudio",
+            "photos": "https://api.unsplash.com/users/eadesstudio/photos",
+            "likes": "https://api.unsplash.com/users/eadesstudio/likes",
+            "portfolio": "https://api.unsplash.com/users/eadesstudio/portfolio",
+            "following": "https://api.unsplash.com/users/eadesstudio/following",
+            "followers": "https://api.unsplash.com/users/eadesstudio/followers"
+          },
+          "profile_image": {
+            "small": "https://images.unsplash.com/profile-1603668573471-f4e7c1f7b3fdimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=32&h=32",
+            "medium": "https://images.unsplash.com/profile-1603668573471-f4e7c1f7b3fdimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=64&h=64",
+            "large": "https://images.unsplash.com/profile-1603668573471-f4e7c1f7b3fdimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=128&h=128"
+          },
+          "instagram_username": "jmeeades",
+          "total_collections": 0,
+          "total_likes": 0,
+          "total_photos": 57,
+          "total_promoted_photos": 37,
+          "total_illustrations": 0,
+          "total_promoted_illustrations": 0,
+          "accepted_tos": true,
+          "for_hire": true,
+          "social": {
+            "instagram_username": "jmeeades",
+            "portfolio_url": "http://eades.studio",
+            "twitter_username": "eadesstudio",
+            "paypal_email": null
+          }
+        },
+        "exif": {
+          "make": "SONY",
+          "model": "ILCE-7RM3",
+          "name": "SONY, ILCE-7RM3",
+          "exposure_time": "1/1250",
+          "aperture": "5.6",
+          "focal_length": "400.0",
+          "iso": 1250
+        },
+        "location": {
+          "name": null,
+          "city": null,
+          "country": null,
+          "position": {
+            "latitude": null,
+            "longitude": null
+          }
+        },
+        "views": 2970998,
+        "downloads": 24395
+      },
+      {
+        "id": "KdZvvyemkPM",
+        "slug": "red-and-brown-rooster-head-in-closeup-shot-KdZvvyemkPM",
+        "alternative_slugs": {
+          "en": "red-and-brown-rooster-head-in-closeup-shot-KdZvvyemkPM",
+          "es": "cabeza-de-gallo-rojo-y-marron-en-primer-plano-KdZvvyemkPM",
+          "ja": "クローズアップショットの赤と茶色のオンドリの頭-KdZvvyemkPM",
+          "fr": "tete-de-coq-rouge-et-brun-en-gros-plan-KdZvvyemkPM",
+          "it": "testa-di-gallo-rossa-e-marrone-in-primo-piano-KdZvvyemkPM",
+          "ko": "빨간색과-갈색-수탉-머리-클로즈업-샷-KdZvvyemkPM",
+          "de": "roter-und-brauner-hahnenkopf-in-nahaufnahme-KdZvvyemkPM",
+          "pt": "cabeca-de-galo-vermelha-e-marrom-em-foto-de-closeup-KdZvvyemkPM"
+        },
+        "created_at": "2016-04-30T14:40:48Z",
+        "updated_at": "2025-04-29T18:07:58Z",
+        "promoted_at": "2016-04-30T14:40:48Z",
+        "width": 5616,
+        "height": 3744,
+        "color": "#592626",
+        "blur_hash": "LSF|PlEM0}-nIqxZkBVuEfR+xGn%",
+        "description": "Rooster side view",
+        "alt_description": "red and brown rooster head in closeup shot",
+        "breadcrumbs": [],
+        "urls": {
+          "raw": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3",
+          "full": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?crop=entropy&cs=srgb&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=85",
+          "regular": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=1080",
+          "small": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=400",
+          "thumb": "https://images.unsplash.com/photo-1462027076063-1ceabb252dbd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8&ixlib=rb-4.0.3&q=80&w=200",
+          "small_s3": "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1462027076063-1ceabb252dbd"
+        },
+        "links": {
+          "self": "https://api.unsplash.com/photos/red-and-brown-rooster-head-in-closeup-shot-KdZvvyemkPM",
+          "html": "https://unsplash.com/photos/red-and-brown-rooster-head-in-closeup-shot-KdZvvyemkPM",
+          "download": "https://unsplash.com/photos/KdZvvyemkPM/download?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8",
+          "download_location": "https://api.unsplash.com/photos/KdZvvyemkPM/download?ixid=M3w3NDQ4MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDU5NTU2OTl8"
+        },
+        "likes": 472,
+        "liked_by_user": false,
+        "current_user_collections": [],
+        "sponsorship": null,
+        "topic_submissions": {
+          "animals": {
+            "status": "approved",
+            "approved_on": "2020-11-03T10:41:03Z"
+          }
+        },
+        "asset_type": "photo",
+        "user": {
+          "id": "Qjmy5alzMGo",
+          "updated_at": "2025-02-26T01:18:47Z",
+          "username": "awesome",
+          "name": "Ashes Sitoula",
+          "first_name": "Ashes",
+          "last_name": "Sitoula",
+          "twitter_username": null,
+          "portfolio_url": null,
+          "bio": "introvert",
+          "location": "Nepal",
+          "links": {
+            "self": "https://api.unsplash.com/users/awesome",
+            "html": "https://unsplash.com/@awesome",
+            "photos": "https://api.unsplash.com/users/awesome/photos",
+            "likes": "https://api.unsplash.com/users/awesome/likes",
+            "portfolio": "https://api.unsplash.com/users/awesome/portfolio",
+            "following": "https://api.unsplash.com/users/awesome/following",
+            "followers": "https://api.unsplash.com/users/awesome/followers"
+          },
+          "profile_image": {
+            "small": "https://images.unsplash.com/profile-1722685586012-530c109255ccimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=32&h=32",
+            "medium": "https://images.unsplash.com/profile-1722685586012-530c109255ccimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=64&h=64",
+            "large": "https://images.unsplash.com/profile-1722685586012-530c109255ccimage?ixlib=rb-4.0.3&crop=faces&fit=crop&w=128&h=128"
+          },
+          "instagram_username": null,
+          "total_collections": 0,
+          "total_likes": 4,
+          "total_photos": 565,
+          "total_promoted_photos": 8,
+          "total_illustrations": 0,
+          "total_promoted_illustrations": 0,
+          "accepted_tos": true,
+          "for_hire": false,
+          "social": {
+            "instagram_username": null,
+            "portfolio_url": null,
+            "twitter_username": null,
+            "paypal_email": null
+          }
+        },
+        "exif": {
+          "make": "Canon",
+          "model": "Canon EOS 5D Mark II",
+          "name": "Canon, EOS 5D Mark II",
+          "exposure_time": "1/400",
+          "aperture": "5.6",
+          "focal_length": "400.0",
+          "iso": 320
+        },
+        "location": {
+          "name": "Kathmandu, Nepal",
+          "city": "Kathmandu",
+          "country": "Nepal",
+          "position": {
+            "latitude": 27.7172453,
+            "longitude": 85.3239605
+          }
+        },
+        "views": 7440191,
+        "downloads": 58296
+      }
+    ]
+    """.data(using: .utf8)!
+
