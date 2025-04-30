32a82a75ee5b5f1e88e113d4adadd77e06a45199
Uriel Ortega <urielortega2509@gmail.com>
2025-04-29 21:29:46 -0600

    Create GalleryView struct.

--- AnimalGallery/Features/Gallery/GalleryView.swift
+++ AnimalGallery/Features/Gallery/GalleryView.swift
@@ -5,3 +5,22 @@
 //  Created by Uriel Ortega on 29/04/25.
 //
 
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            
+            Text("Hello, GalleryView!")
+                .padding()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    GalleryView()
+}
